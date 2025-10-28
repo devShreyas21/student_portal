ee325233c76adbf692917b6a96b30911b2680ada
devShreyas21 <devshreyas21@gmail.com>
2025-10-28 15:22:44 +0530

    changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ import { swaggerSpec, swaggerUi } from "./swagger.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"], credentials: true }));
 app.use(express.json());
 
 // ✅ Swagger Docs route
